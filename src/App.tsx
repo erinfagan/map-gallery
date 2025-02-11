f381379404607bc5eb520f786907b0d7a6aaa14b
KittenMitt0ns <158006637+erinfagan@users.noreply.github.com>
2025-02-11 18:37:32 -0500

    chart is working

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,52 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
 import { parse } from 'exifr';
+
 import { AdvancedMarker, APIProvider, Map, MapCameraChangedEvent, Pin } from '@vis.gl/react-google-maps';
 import './App.css'
 
-const _URLS:string[] = ["0.jpg", "1.jpg", "2.jpg"]; 
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
-type GPSInfo = { image: string, location: google.maps.LatLngLiteral, alt: number }
+const _URLS:string[] = ["0.jpg", "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg", "10.jpg", "11.jpg", "12.jpg", "13.jpg", "14.jpg", "15.jpg"]; 
+
+type GPSInfo = { ind:number, label:string, image: string, location: google.maps.LatLngLiteral, alt: number, cd: number };
 
 function App() {
-  const [imageUrl,setImageUrl] = useState(_URLS[0]);
-  const [imageData, setImageData] = useState<GPSInfo[]>();//for active image
+  const [activeImage,setActiveImage] = useState(_URLS[0]);
+  const [mapData, setmapData] = useState<GPSInfo[]>();
 
   useEffect(() => {
     let coords:GPSInfo[] = [];
-    async function processFiles() {
-      await Promise.all(_URLS.map(async item => {
-        let data = await parse(item, {gps:true, translateKeys: true, translateValues:true});
-        let pos:google.maps.LatLngLiteral = {lat:data.latitude,lng:data.longitude};
-        coords.push({image:item, location:pos, alt: data.GPSAltitude});
-      }));
 
-      setImageData(coords);
-      if(imageData!==undefined){
-        console.log('done: '+imageData[0].location);
+    async function processFiles() {
+      await Promise.all(_URLS.map(async (item, i) => {
+        let data = await parse(item, {pick:['CreateDate', 'GPSLatitude', 'GPSLongitude', 'GPSAltitude'], reviveValues: true, translateKeys: true});
+        let pos:google.maps.LatLngLiteral = {lat:data.latitude,lng:data.longitude};
+        let picName = data.CreateDate instanceof Date ? data.CreateDate.toLocaleString('en-us', {weekday: 'short'}): item;
+        coords.push({ind: i, label: picName, image:item, location:pos, alt: data.GPSAltitude, cd: data.CreateDate});
+      }));
+      coords.sort((a,b) => a.ind-b.ind);
+      setmapData(coords);
+      if(mapData!==undefined){
+        console.log('done: '+mapData[0].location);
       }
     }
     processFiles();
@@ -32,20 +56,25 @@ function App() {
     <>
       <div className="container">
         <div className="image">
-          <img id="photo" src={imageUrl} alt='image' />
+          <img id="photo" src={activeImage} alt='image' />
         </div>
-        {imageData !== undefined && (
-          <div className="card">                        
+        {mapData !== undefined && (
+          <div className="card"> 
+            <div className="map">                       
               <APIProvider apiKey={'AIzaSyA0XFbVGVO7sd0FGQFDmtzO7ZgFrenMWbA'} onLoad={() => console.log('Maps API has loaded.')}>
-                  <Map 
-                      defaultZoom={10}
-                      defaultCenter={ imageData[0].location }                      
-                      onCameraChanged={ (ev: MapCameraChangedEvent) => console.log('camera changed:', ev.detail.center, 'zoom:', ev.detail.zoom)}
-                      mapId='85777689c84f7376'
-                      mapTypeId={'terrain'}>
-                      <PoiMarkers pois={imageData} handleClick={setImageUrl}/>
-                  </Map> 
-              </APIProvider>
+                <Map 
+                    defaultZoom={10}
+                    defaultCenter={ mapData[0].location }                      
+                    onCameraChanged={ (ev: MapCameraChangedEvent) => console.log('camera changed:', ev.detail.center, 'zoom:', ev.detail.zoom)}
+                    mapId='85777689c84f7376'
+                    mapTypeId={'terrain'}>
+                    <PoiMarkers pois={mapData} handleClick={setActiveImage}/>
+                </Map> 
+              </APIProvider>   
+            </div>
+            <div className="chart">           
+              <LineChart data={mapData} handleClick={setActiveImage} />              
+            </div>
           </div>
         )}
       </div>
@@ -53,12 +82,61 @@ function App() {
   )
 }
 
+const LineChart = (props:{data: GPSInfo[], handleClick: (which:string) => void}) => {
+  const chartRef = useRef(null);
+  const [chartData, setChartData] = useState();
+
+  //set data
+  useEffect(() => {
+    const chart = chartRef.current;
+    if(!chart) return;
+    //setChartData
+
+  },[]);
+
+  const options = {
+    responsive: true,
+    elements: {
+      point: {
+        radius:5
+      }
+    },
+    plugins: {
+      legend: {
+        position: 'top' as const
+      },
+      title: {
+        display: true,
+        text: 'Elevation Change Per Photo'
+      },
+    }
+  };
+  
+  const data = {
+    labels: props.data.map((item) => item.image),
+    datasets: [
+      {
+        label: 'Set 1',
+        data: props.data.map((item) => item.alt),
+        borderColor: 'rgb(18, 79, 201)',
+        backgroundColor: 'rgba(201, 18, 201, 1)'
+      }
+    ]
+  };
+
+
+  return (
+    <Line ref={chartRef} options={options} data={data} />
+  )
+}
+
+//display info per click, date, altitude, distance/altitude gain-loss from previous
 const PoiMarkers = (props: {pois: GPSInfo[], handleClick: (which:string) => void }) => {
   return (
     <>
     {props.pois.map((poi: GPSInfo) => (
       <AdvancedMarker key={poi.image} position={poi.location} title={poi.image} onClick={()=>props.handleClick(poi.image)}>
-        <Pin background={'#FBBC04'} glyphColor={'#000'}borderColor={'#000'} />
+        <Pin background={'#d62111'} glyph={poi.ind.toString()} glyphColor={'#FFF'} borderColor={'#7c170d'} />
       </AdvancedMarker>
     ))}
     </>
